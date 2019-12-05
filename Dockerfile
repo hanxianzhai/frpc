45ba1aa8ece1787ebf0f6db9e5ec2777918aec7b
hanxianzhai <hanxianzhai@gmail.com>
2019-12-05 16:44:23 +0800

    entrypoint

--- Dockerfile
+++ Dockerfile
@@ -8,14 +8,10 @@ RUN set -x && \
         wget --no-check-certificate https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_amd64.tar.gz && \ 
         tar xzf frp_${FRP_VERSION}_linux_amd64.tar.gz && \
         mkdir -p /opt/frp && \
-        cd frp_${FRP_VERSION}_linux_amd64 && \
-        mv frpc /usr/local/bin && \
+        mv frp_${FRP_VERSION}_linux_amd64/frpc /usr/local/bin/ && \
         chmod +x /usr/local/bin/frpc && \
-        mv frpc.ini /opt/frp/frpc.ini && \
-        cd .. && \
-        rm -rf *.tar.gz && \
-        rm -rf frp_${FRP_VERSION}_linux_amd64
+        rm -rf -rf /var/cache/apk/* ~/.cache frp_${FRP_VERSION}_linux_amd64.tar.gz frp_${FRP_VERSION}_linux_amd64
 
-VOLUME /opt/conf
+VOLUME /opt/frp
 
 CMD frpc -c /opt/frp/frpc.ini
---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#########################################################################
+
+#########################################################################
+__readINI() {
+ INIFILE=$1; SECTION=$2; ITEM=$3
+ _readIni=`awk -F '=' '/\['$SECTION'\]/{a=1}a==1&&$1~/'$ITEM'/{print $2;exit}' $INIFILE`
+echo ${_readIni}
+}
+
+set -e
+FRPC_BIN="/usr/local/bin/frpc"
+FRPC_CONF="/opt/frpc/frpc.ini"
+FRPC_LOG=$(__readINI ${FRPC_CONF} common log_file)
+
+str_log_level=${str_log_level:-info}   # set log level: debug, info, warn, error
+
+
+if [ ! -r ${FRPC_CONF} ]; then
+    echo "config file ${FRPC_CONF} not found"
+    exit 1
+fi
+[ -z ${FRPC_LOG} ] && echo "Log file not setting,exit!" && exit 1
+touch ${FRPC_LOG} > /dev/null 2>&1
+echo "Starting frpc $(${FRPC_BIN} -v) ..."
+${FRPC_BIN} -c ${FRPC_CONF} &
+exec "tail" -f ${FRPC_LOG}
